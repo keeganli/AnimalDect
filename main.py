87f9760c010666fb20ee41e1574550e68d013fa6
LCL <2860542076@qq.com>
2022-07-19 18:59:29 +0800

    完成本地图片的提交

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-from bs4 import ResultSet
 from flask import Flask, request, redirect, url_for, render_template
 from regex import P
+import shutil
 
 app = Flask(__name__)
 
@@ -11,19 +11,25 @@ def hello_world():
     return render_template('index.html')
 
 
-# 从前端接受文件并保存到本地
-@app.route('/distinguish/', methods=['POST', 'GET'])
+# 从前端接受文件并识别
+@app.route('/distinguish/', methods=['POST'])
 def upload_file():
+    #首先清空临时文件夹
+    shutil.rmtree('static/tmp')
+    os.makedirs('static/tmp')
+
+    #保存文件
     file = request.files['file']
-    # file.save('tmp/' + file.filename)
+    file.save('static/tmp/' + file.filename)
 
     #调用训练模型并返回结果
     filename = file.filename
 
-    commmand = "python distinguish.py " + filename
+    # commmand = "python distinguish.py " + filename
     # result = os.popen(commmand).read()
     result = "识别结果"
-    return render_template('result.html', result=result)
+
+    return render_template('result.html', filename=filename, result=result)
 
 
 if __name__ == '__main__':
