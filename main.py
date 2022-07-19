4547cd006ddb15015a5e0d13e87b3d790dd89f83
LCL <2860542076@qq.com>
2022-07-19 18:28:09 +0800

    框架的修缮

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from bs4 import ResultSet
-from flask import Flask, request, redirect, url_for, render_template, flash
+from flask import Flask, request, redirect, url_for, render_template
 from regex import P
 
 app = Flask(__name__)
@@ -12,18 +12,18 @@ def hello_world():
 
 
 # 从前端接受文件并保存到本地
-@app.route('/distinguish/', methods=['POST'])
+@app.route('/distinguish/', methods=['POST', 'GET'])
 def upload_file():
-
     file = request.files['file']
-    file.save('tmp/' + file.filename)
+    # file.save('tmp/' + file.filename)
 
     #调用训练模型并返回结果
     filename = file.filename
 
     commmand = "python distinguish.py " + filename
-    result = os.popen(commmand).read()
-    return render_template('./result.html', result=result)
+    # result = os.popen(commmand).read()
+    result = "识别结果"
+    return render_template('result.html', result=result)
 
 
 if __name__ == '__main__':
