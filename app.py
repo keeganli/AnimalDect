1952b65063cf339aaf2ea06d099b4f56cff8c6e8
LCL <2860542076@qq.com>
2022-07-20 09:55:31 +0800

    BUG修复
    修复少部分BUG

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from ast import literal_eval
 import os
 from flask import Flask, request, redirect, url_for, render_template
 import shutil
+
 app = Flask(__name__)
 
 
@@ -15,22 +17,32 @@ def upload_file():
     #首先清空临时文件夹
     shutil.rmtree('static/tmp')
     os.makedirs('static/tmp')
-    print("1ewdec")
-    # #保存文件
-    # file = request.files['file']
-    # file.save('static/tmp/' + file.filename)
-    # for file in request.files.getlist('file'):
-    #     print(file)
-    #调用训练模型并返回结果
-    filename = file.filename
 
-    commmand = "python distinguish.py " + filename
-    result="识别失败"
+    #从前端获取文件集并保存，并将文件名保存到文件名列表中
+    files = request.files.getlist('file')
+    filenames = []
+    for file in files:
+        file.save('static/tmp/' + file.filename)
+        filenames.append(file.filename)
+
+    #调用训练模型并返回结果
+    commmand = "python distinguish.py "
+
+    #获取结果并返回
     result = os.popen(commmand).read()
-    # result = os.system(commmand)
-    # result = "识别结果"
-    print(result)
-    return render_template('result.html', filename=filename, result=result)
+    results = literal_eval(result)
+    print(results)
+    dicti = dict(zip(filenames, results))
+
+    # //print(endResult)
+    # //print(type(endResult))
+
+    #filenames:图片名称列表 results:识别结果列表 一一对应
+    return render_template('result.html',
+                           filenames=filenames,
+                           results=results,
+                           dicti=dicti)
+
 
 if __name__ == '__main__':
     app.run()
--- distinguish.py
+++ distinguish.py
@@ -7,25 +7,28 @@ from tensorflow import keras
 
 
 def main():
-    filename = sys.argv[1]  #filename 就是用户上传的文件名
-    # filename='3.jpg'
+
+    #读取tmp文件夹中读取所有文件并设定大小
+    filepath = 'static/tmp/'
+    imgs = []
+    for im in glob.glob(filepath + '*.jpg'):
+        img = cv2.imread(im)
+        img = cv2.resize(img, (64, 64))
+        imgs.append(img)
+    image = np.asarray(imgs, dtype='float32')
+
     # 加载模型
     model = keras.models.load_model('my_model.h5')
 
-    filepath = 'static\\tmp\\' + filename
-
-    #读取文件并设定大
-    imgs = []
-    img = cv2.imread(filepath)
-    img = cv2.resize(img, (64, 64))
-    imgs.append(img)
-    image = np.asarray(imgs)
-
     # 模型预测,输出预测结果数组（待定，应当输出字符串）
     predictions = model.predict(image)
-    print(predictions)
 
-    #删除临时文件
+    # 输出预测结果
+    outcomes = ['狗', '马', '大象', '蝴蝶', '鸡', '猫', '羊']
+    result = []
+    for index in range(len(predictions)):
+        result.append(outcomes[predictions[index].tolist().index(1.0)])
+    print(result)
 
 
 if __name__ == '__main__':
